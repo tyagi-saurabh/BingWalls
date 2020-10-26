10653b3bfb5442b6450ee5c2345793eb88f2125c
tyagi-saurabh <masterchief.tyagi@hotmail.com>
2020-10-27 00:29:59 +0530

    brought down all the constants in one file

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const ktitleTextStyle =
+    TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 30.0);
+const kcopyrightTextStyle =
+    TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 15.0);
+const String partAddress =
+    "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=";
