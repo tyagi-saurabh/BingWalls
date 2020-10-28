d0368406c503157a71a0f0f2b2923ab6a419854e
tyagi-saurabh <masterchief.tyagi@hotmail.com>
2020-10-28 19:37:17 +0530

    major code reafctoring

---
+++ lib/screens/body.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+    @required this.url,
+    @required this.title,
+    @required this.copyright,
+  }) : super(key: key);
+
+  final String url;
+  final String title;
+  final String copyright;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.network(
+          url,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+          color: Colors.transparent,
+          child: ExpansionTile(
+            trailing: Icon(
+              Icons.expand_more,
+              color: Colors.white,
+              size: 30.0,
+            ),
+            title: Text(
+              title,
+              style: ktitleTextStyle,
+            ),
+            children: <Widget>[
+              Text(
+                copyright,
+                style: kcopyrightTextStyle,
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
